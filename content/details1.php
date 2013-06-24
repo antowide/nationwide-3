7229e7d60b3642aea3ab7cf75998ebe78e54af2a
Anthony Owide <antowide@hotmail.com>
2013-06-24 23:42:14 +0100

    Add carousel images, and details page markup.

--- content/details1.php
+++ content/details1.php
@@ -1,5 +1,5 @@
 <?php include('includes/head-details.php'); ?>
-<?php include('includes/headers/header.php'); ?>
+<?php include('includes/headers/header-details.php'); ?>
 
 
   <div class="container">
@@ -9,6 +9,56 @@
     	<div class="span7">
   			<h2>Property Details for 666 Elm Street</h2>
 	        <p>This page is currently under construction, and should be live soon.</p>
+            
+            
+            
+            
+            
+            
+            
+            <div class="container">
+			
+			<div class="content">
+				<h1 style="font-family:Arial, Helvetica, sans-serif;">Responsive Image Gallery <span>A jQuery image gallery with a thumbnail carousel</span></h1>
+				<div id="rg-gallery" class="rg-gallery">
+					<div class="rg-thumbs">
+						<!-- Elastislide Carousel Thumbnail Viewer -->
+						<div class="es-carousel-wrapper">
+							<div class="es-nav">
+								<span class="es-nav-prev">Previous</span>
+								<span class="es-nav-next">Next</span>
+							</div>
+							<div class="es-carousel">
+								<ul>
+									<li><a href="#"><img src="images/thumbs/1.jpg" data-large="images/1.jpg" alt="image01" data-description="From off a hill whose concave womb reworded" /></a></li>
+									<li><a href="#"><img src="images/thumbs/2.jpg" data-large="images/2.jpg" alt="image02" data-description="A plaintful story from a sistering vale" /></a></li>
+									<li><a href="#"><img src="images/thumbs/3.jpg" data-large="images/3.jpg" alt="image03" data-description="A plaintful story from a sistering vale" /></a></li>
+									<li><a href="#"><img src="images/thumbs/4.jpg" data-large="images/4.jpg" alt="image04" data-description="My spirits to attend this double voice accorded" /></a></li>
+									<li><a href="#"><img src="images/thumbs/5.jpg" data-large="images/5.jpg" alt="image05" data-description="And down I laid to list the sad-tuned tale" /></a></li>
+									<li><a href="#"><img src="images/thumbs/6.jpg" data-large="images/6.jpg" alt="image06" data-description="Ere long espied a fickle maid full pale" /></a></li>
+									<li><a href="#"><img src="images/thumbs/7.jpg" data-large="images/7.jpg" alt="image07" data-description="Tearing of papers, breaking rings a-twain" /></a></li>
+									<li><a href="#"><img src="images/thumbs/8.jpg" data-large="images/8.jpg" alt="image08" data-description="Storming her world with sorrow's wind and rain" /></a></li>
+									<li><a href="#"><img src="images/thumbs/9.jpg" data-large="images/9.jpg" alt="image09" data-description="Upon her head a platted hive of straw" /></a></li>
+									<li><a href="#"><img src="images/thumbs/10.jpg" data-large="images/10.jpg" alt="image10" data-description="Which fortified her visage from the sun" /></a></li>
+								</ul>
+							</div>
+						</div>
+						<!-- End Elastislide Carousel Thumbnail Viewer -->
+					</div><!-- rg-thumbs -->
+				</div><!-- rg-gallery -->
+				
+			</div><!-- content -->
+		</div><!-- container -->
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         </div>
     </div>
     
---
+++ content/includes/headers/header-details.php
@@ -0,0 +1,43 @@
+
+<!-- [header.php] -->
+
+<body>
+    <?php include('includes/navbar-wrapper.php'); ?>
+            <?php include('includes/nav-brand.php'); ?>
+            <div class="nav-collapse collapse">
+              <ul class="nav">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li class="dropdown active">
+                  <a href="prop-search.php" class="dropdown-toggle" data-toggle="dropdown">Property Search <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                  	<li class="nav-header">Residential</li>
+                    <li><a href="results1.php">Residential Property: To Buy</a></li>
+                    <li><a href="results1.php">Residential Property: To Rent</a></li>
+                    <li class="divider"></li>
+                    <li class="nav-header">Commercial</li>
+                    <li><a href="results1.php">Commercial Property: To Buy</a></li>
+                    <li><a href="results1.php">Commercial Property: To Rent</a></li>
+                  </ul>
+                </li>
+              	<li><a href="finance.php">Finance</a></li>
+                <li><a href="links.php">Links</a></li>
+                <li><a href="testimonials.php">Testimonials</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div><!-- /.navbar-inner -->
+        </div><!-- /.navbar -->
+        
+        <ul class="breadcrumb span11">
+          <li><a href="index.php">Home</a> <span class="divider">/</span></li>
+          <li class="active">About Us</li>
+          <li class="google-search">
+          <?php include('includes/search.php'); ?>
+          </li>
+        </ul>
+  		</div> <!-- /.container -->
+    </div>
+    
+<!-- [/header.php] -->    
+    
