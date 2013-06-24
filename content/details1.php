6a89cd21ca489e31340d396ae832a6f6473d3060
Anthony Owide <antowide@hotmail.com>
2013-06-25 00:38:25 +0100

    Fix carousel image path data-attributes.

--- content/details1.php
+++ content/details1.php
@@ -9,14 +9,6 @@
     	<div class="span7">
   			<h2>Property Details for 666 Elm Street</h2>
 	        <p>This page is currently under construction, and should be live soon.</p>
-            
-            
-            
-            
-            
-            
-            
-            
 			
 			<div class="content">
 				<div id="rg-gallery" class="rg-gallery">
@@ -29,16 +21,16 @@
 							</div>
 							<div class="es-carousel">
 								<ul>
-									<li><a href="#"><img src="img/carousel-images/thumbs/1.jpg" data-large="images/1.jpg" alt="image01" data-description="From off a hill whose concave womb reworded" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/2.jpg" data-large="images/2.jpg" alt="image02" data-description="A plaintful story from a sistering vale" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/3.jpg" data-large="images/3.jpg" alt="image03" data-description="A plaintful story from a sistering vale" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/4.jpg" data-large="images/4.jpg" alt="image04" data-description="My spirits to attend this double voice accorded" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/5.jpg" data-large="images/5.jpg" alt="image05" data-description="And down I laid to list the sad-tuned tale" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/6.jpg" data-large="images/6.jpg" alt="image06" data-description="Ere long espied a fickle maid full pale" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/7.jpg" data-large="images/7.jpg" alt="image07" data-description="Tearing of papers, breaking rings a-twain" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/8.jpg" data-large="images/8.jpg" alt="image08" data-description="Storming her world with sorrow's wind and rain" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/9.jpg" data-large="images/9.jpg" alt="image09" data-description="Upon her head a platted hive of straw" /></a></li>
-									<li><a href="#"><img src="img/carousel-images/thumbs/10.jpg" data-large="images/10.jpg" alt="image10" data-description="Which fortified her visage from the sun" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/1.jpg" data-large="img/carousel-images/1.jpg" alt="image01" data-description="From off a hill whose concave womb reworded" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/2.jpg" data-large="img/carousel-images/2.jpg" alt="image02" data-description="A plaintful story from a sistering vale" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/3.jpg" data-large="img/carousel-images/3.jpg" alt="image03" data-description="A plaintful story from a sistering vale" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/4.jpg" data-large="img/carousel-images/4.jpg" alt="image04" data-description="My spirits to attend this double voice accorded" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/5.jpg" data-large="img/carousel-images/5.jpg" alt="image05" data-description="And down I laid to list the sad-tuned tale" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/6.jpg" data-large="img/carousel-images/6.jpg" alt="image06" data-description="Ere long espied a fickle maid full pale" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/7.jpg" data-large="img/carousel-images/7.jpg" alt="image07" data-description="Tearing of papers, breaking rings a-twain" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/8.jpg" data-large="img/carousel-images/8.jpg" alt="image08" data-description="Storming her world with sorrow's wind and rain" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/9.jpg" data-large="img/carousel-images/9.jpg" alt="image09" data-description="Upon her head a platted hive of straw" /></a></li>
+									<li><a href="#"><img src="img/carousel-images/thumbs/10.jpg" data-large="img/carousel-images/10.jpg" alt="image10" data-description="Which fortified her visage from the sun" /></a></li>
 								</ul>
 							</div>
 						</div>
@@ -47,16 +39,6 @@
 				</div><!-- rg-gallery -->
 				
 			</div><!-- content -->
-		
-            
-            
-            
-            
-            
-            
-            
-            
-            
             
         </div>
     </div>
