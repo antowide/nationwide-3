8781abdf69a002c43c3580f1023d9f247ca3089d
Anthony Owide <antowide@hotmail.com>
2013-07-01 22:04:56 +0100

    Restructure CSS files into /CSS directory.

--- content/includes/head.php
+++ content/includes/head.php
@@ -9,8 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="docs/assets/css/bootstrap.css" rel="stylesheet">
-	<link href="docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Domine' rel='stylesheet' type='text/css'>
     
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
